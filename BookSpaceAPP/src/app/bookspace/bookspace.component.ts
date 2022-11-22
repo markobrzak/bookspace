3e634f82aa30dc35576cc6d38793aa0cc3b396d7
Marko Brzak <marko.brzak.5@gmail.com>
2022-11-22 19:24:12 +0100

    Added bootstrap and initial component/service.

---
+++ BookSpaceAPP/src/app/bookspace/bookspace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookspace',
+  templateUrl: './bookspace.component.html',
+  styleUrls: ['./bookspace.component.scss']
+})
+export class BookspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
