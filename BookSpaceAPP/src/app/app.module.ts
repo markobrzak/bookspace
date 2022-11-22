ed0b8354993583c7f77a3a3736d68a2c9b9b3747
Marko Brzak <marko.brzak.5@gmail.com>
2022-11-22 19:45:39 +0100

    Imported Favicon.

--- BookSpaceAPP/src/app/app.module.ts
+++ BookSpaceAPP/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BookspaceComponent } from './bookspace/bookspace.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { BookspaceComponent } from './bookspace/bookspace.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
