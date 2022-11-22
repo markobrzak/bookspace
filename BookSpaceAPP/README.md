68990fda0fb1be777f5788bc274f15eb023a6df3
Marko Brzak <marko.brzak.5@gmail.com>
2022-11-22 19:14:15 +0100

    Downgraded Angular CLI to 14.1.0.

--- BookSpaceAPP/README.md
+++ BookSpaceAPP/README.md
@@ -1,6 +1,6 @@
 # BookSpaceAPP
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.0.
 
 ## Development server
 
--- BookSpaceAPP/src/app/app.module.ts
+++ BookSpaceAPP/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BookspaceComponent } from './bookspace/bookspace.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BookspaceComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
